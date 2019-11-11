37f8a92a20f60bf74cbcadfc521fa327c978bae6
MarinaShakhova1946 <shak.marina15@gmail.com>
2019-11-11 18:21:42 +0300

    My first commit

---
+++ src/main/java/ru/marina/spsuace/telegramhockeybot/MyBot.java
@@ -0,0 +1,66 @@
+package ru.marina.spsuace.telegramhockeybot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.ApiContext;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+public class MyBot extends TelegramLongPollingBot {
+
+    public static final String USERNAME = "@Hockey_team_bot";
+    public static final String TOKEN = "980065316:AAHqnnc6HRKXEemwA0kfIIDAr719e-REO3Q";
+
+    public MyBot(DefaultBotOptions botOptions) {
+        super(botOptions);
+    }
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+            botOptions.setProxyHost("177.8.226.254");
+            botOptions.setProxyPort(8080);
+            botOptions.setProxyType(DefaultBotOptions.ProxyType.HTTP);
+            telegramBotsApi.registerBot(new MyBot(botOptions));
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        update.getUpdateId();
+        if (update.hasMessage()) {
+            SendMessage sendMessage = new SendMessage().setChatId((update.getMessage().getChatId()));
+            if (update.getMessage().getText().equals("Who is the champion?")) {
+                sendMessage.setText("SKA IS THE BEST!!!");
+            } else {
+                sendMessage.setText("If you like hockey, ask who is the champion:)");
+            }
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return USERNAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return TOKEN;
+    }
+}
+
+
+
